b52951fdd400870df6a77e707e84e2ca89ac2638
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-06-08 18:07:46 -0400

    tabs 4 wide

--- init.lua
+++ init.lua
@@ -68,6 +68,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- I like four character tabs
+vim.opt.tabstop = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
